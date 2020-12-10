cea0f1e9f888ad87702d418559e2eea35fbd35aa
AXON <aminurfisa3@gmail.com>
2020-12-10 08:29:10 +0600

    Many things reconfigured

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use execute::Execute; // For simplifying external command execution.
 use nix::unistd;
 use std::env::set_var; // For exporting environmen vars.
 use std::ffi::CString;
@@ -6,8 +5,10 @@ use std::fs; // For getting fuctions for dir creation.
 use std::os::unix::fs::{symlink, PermissionsExt};
 use std::path::Path; // For working with file existences.
 use std::process::{exit, Command};
-use sys_mount::{Mount, MountFlags, SupportedFilesystems};
 
+/*
+use sys_mount::{Mount, MountFlags, SupportedFilesystems};
+use execute::Execute; // For simplifying external command execution.
 pub fn bind_mount(source_file: &str, target_file: &str) {
     // Fetch a list of supported file systems.
     // When mounting, a file system will be selected from this.
@@ -28,13 +29,7 @@ pub fn bind_mount(source_file: &str, target_file: &str) {
     }
 }
 
-pub fn executev(args: &[&str]) {
-    let args: Vec<CString> = args
-        .iter()
-        .map(|t| CString::new(*t).expect("not a proper CString"))
-        .collect();
-    unistd::execv(&args[0], &args).expect("failed");
-}
+
 
 pub fn run_externc(
     executable: &str,
@@ -59,97 +54,114 @@ pub fn run_externc(
         }
     }
 }
+*/
+
+pub fn executev(args: &[&str]) {
+    let args: Vec<CString> = args
+        .iter()
+        .map(|t| CString::new(*t).expect("not a proper CString"))
+        .collect();
+    unistd::execv(&args[0], &args).expect("failed");
+}
 
 pub fn chmod(file: &str, mode: u32) {
     fs::set_permissions(file, fs::Permissions::from_mode(mode)).unwrap();
 }
 
 pub fn init_magisk() {
-    // Overwrite init.superuser.rc
+    // Overwrite root config files
     let superuser_config = "/init.superuser.rc";
-    let superuser_data = "
-# su daemon
-service su_daemon /sbin/magisk --daemon
-    seclabel u:r:su:s0
-    oneshot
+    let magisk_config = "/sbin/.magisk/config";
+    let magisk_bin = "/sbin/magisk";
 
-on property:persist.sys.root_access=0
-    start su_daemon
-
-on property:persist.sys.root_access=2
-    start su_daemon
-
-on property:persist.sys.root_access=1
-    start su_daemon
-
-on property:persist.sys.root_access=3
-    start su_daemon
-
-on post-fs-data
-    exec u:r:su:s0 -- /sbin/magisk --post-fs-data
-
-on property:sys.boot_completed=1
-    exec u:r:su:s0 -- /sbin/magisk --service
-    exec u:r:su:s0 -- /sbin/magisk --boot-complete
-";
-
-    match fs::write(superuser_config, superuser_data) {
+    match fs::write(superuser_config, include_bytes!("config/su")) {
         Ok(_) => {
             chmod(superuser_config, 0o755);
         }
         Err(why) => {
-            eprintln!("Error: Failed to overwrite superuser config file: {}", why);
+            eprintln!("Error: Failed to write superuser config file: {}", why);
+            exit(1);
+        }
+    }
+
+    match fs::write(magisk_config, include_bytes!("config/magisk")) {
+        Ok(_) => {
+            chmod(magisk_config, 0o755);
+        }
+        Err(why) => {
+            eprintln!("Error: Failed to write magisk config file: {}", why);
             exit(1);
         }
     }
 
     // Extract magiskinit and set it up
-    let magisk_bin = include_bytes!("magiskinit");
-    match fs::write("/sbin/magiskinit", magisk_bin) {
-        Ok(_) => {
-            // Extract magisk bin from magiskinit
-            chmod("/sbin/magiskinit", 0o777);
-            run_externc(
-                "/sbin/magiskinit",
-                "-x",
-                "magisk",
-                "/sbin/magisk",
-                "Error: Failed to extract magisk from magiskinit",
-            );
+    //// Using x86_64 abi one by default.
+    //// You can change it to `magisk` too.
 
-            // Link magisk applets
-            for file in ["su", "resetprop", "magiskhide"].iter() {
-                if !Path::new(&format!("{}{}", "/sbin/", file)).exists() {
-                    match symlink("/sbin/magisk", format!("{}{}", "/sbin/", file)) {
-                        Ok(_) => {}
-                        Err(why) => {
-                            eprintln!("Error: Failed to symlink for {}: {}", file, why);
-                            exit(1);
-                        }
-                    }
-                }
+    let _magisk_bin_data_x86 = include_bytes!("bin/magisk");
+    let _magisk_bin_data_x64 = include_bytes!("bin/magisk64");
+
+    if Path::new("/system/lib64").exists() {
+        match fs::write(magisk_bin, _magisk_bin_data_x64) {
+            Ok(_) => {}
+
+            Err(why) => {
+                eprintln!("Error: Failed to extract magisk: {}", why);
+                exit(1);
             }
+        }
+    } else {
+        match fs::write(magisk_bin, _magisk_bin_data_x86) {
+            Ok(_) => {}
 
-            for dir in [
-                "/data/adb/modules",
-                "/data/adb/post-fs-data.d",
-                "/data/adb/services.d",
-            ]
-            .iter()
-            {
-                match fs::create_dir_all(dir) {
-                    Ok(_) => {}
-                    Err(_) => {
-                        eprintln!("Error: Failed to create {} dir", dir);
-                        exit(1);
-                    }
+            Err(why) => {
+                eprintln!("Error: Failed to extract magisk: {}", why);
+                exit(1);
+            }
+        }
+    }
+
+    chmod("/sbin/magisk", 0o777);
+
+    /*
+    // Extract magisk bin from magiskinit
+    chmod("/sbin/magiskinit", 0o777);
+
+    run_externc(
+        "/sbin/magiskinit",
+        "-x",
+        "magisk",
+        "/sbin/magisk",
+        "Error: Failed to extract magisk from magiskinit",
+    );
+    */
+
+    // Link magisk applets
+    for file in ["su", "resetprop", "magiskhide"].iter() {
+        if !Path::new(&format!("{}{}", "/sbin/", file)).exists() {
+            match symlink("/sbin/magisk", format!("{}{}", "/sbin/", file)) {
+                Ok(_) => {}
+                Err(why) => {
+                    eprintln!("Error: Failed to symlink for {}: {}", file, why);
+                    exit(1);
                 }
             }
         }
+    }
 
-        Err(why) => {
-            eprintln!("Error: Failed to extract magiskinit: {}", why);
-            exit(1);
+    for dir in [
+        "/data/adb/modules",
+        "/data/adb/post-fs-data.d",
+        "/data/adb/services.d",
+    ]
+    .iter()
+    {
+        match fs::create_dir_all(dir) {
+            Ok(_) => {}
+            Err(why) => {
+                eprintln!("Error: Failed to create {} dir: {}", dir, why);
+                exit(1);
+            }
         }
     }
 }
@@ -165,27 +177,46 @@ pub fn job() {
     let mirror_dir = [
         format!("{}{}", bin_dir, "/.magisk/mirror/data"),
         format!("{}{}", bin_dir, "/.magisk/mirror/system"),
+        format!("{}{}", bin_dir, "/.magisk/modules"),
+        // format!("{}{}", bin_dir, "/.magisk/block"),
     ];
 
     for dir in mirror_dir.iter() {
         match fs::create_dir_all(dir) {
             Ok(_) => {}
-            Err(_) => {
-                eprintln!("Error: Failed to create {} dir", dir);
+            Err(why) => {
+                eprintln!("Error: Failed to create {} dir: {}", dir, why);
                 exit(1);
             }
         }
     }
 
     //// Bind data and system mirrors in /sbin
-    bind_mount("/data", &mirror_dir[0]);
-    bind_mount("/system", &mirror_dir[1]);
+    match fs::write("/sbin/mount", include_bytes!("bin/mount")) {
+        Ok(_) => {
+            chmod("/sbin/mount", 0o777);
+        }
+
+        Err(why) => {
+            eprintln!("Error: Failed to extract mount helper: {}", why);
+            exit(1);
+        }
+    }
+    let mut miror_num = 0;
+    for mirror in ["/data", "/system"].iter() {
+        Command::new("/sbin/mount")
+            .args(&["--bind", mirror, &mirror_dir[miror_num]])
+            .spawn()
+            .expect("Error: Failed to mount a mirror for magisk");
+        miror_num += 1;
+    }
 
     //// Initialize magisk
     init_magisk();
 
+    /*
     // Now let's deal with selinux if needed
-    if Path::new("/sys/fs/selinux").is_dir() {
+    if Path::new("/sys/fs/selinux").exists() {
         // Fix se-context
         run_externc(
             "/system/bin/chcon",
@@ -214,11 +245,12 @@ pub fn job() {
             );
         }
     }
+    */
 
     // Swtitch process to OS init.
     let init_real = "/init.real";
     if Path::new(init_real).exists() {
-        executev(&["/init.real"]);
+        executev(&[init_real]);
     }
 }
 
